ad5522949ad4a0bd54f928254594876587aec50a
dtconfect <dtconfect@gmail.com>
2017-01-10 19:36:35 +0000

    Content update 19:36 10/01/2017 GMT

--- conf/users.auth.php
+++ conf/users.auth.php
@@ -9,4 +9,4 @@
 # login:passwordhash:Real Name:email:groups,comma,seperated
 
 
-dtconfect:$1$7BYkD5ZH$a0RdVlZRGImfssUXmbPgx1:Lark Irienys:dtconfect@gmail.com:admin,user
+dtconfect:$1$ynzp3XYJ$JbIRoqYx8JYbeJhPzormJ1:Lark Irienys:dtconfect@gmail.com:admin,user
