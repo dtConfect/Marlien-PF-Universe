409c3ebeda1074eb1d01cb64b2c596a879947e76
dtconfect <dtconfect@gmail.com>
2017-01-10 19:01:22 +0000

    First Commit

---
+++ conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+dtconfect:$1$7BYkD5ZH$a0RdVlZRGImfssUXmbPgx1:Lark Irienys:dtconfect@gmail.com:admin,user
