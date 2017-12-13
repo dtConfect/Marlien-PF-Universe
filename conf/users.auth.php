aa590ed9d7da2268d66ca5ed4e6ab7a807e3e5ba
Dan Taylor <dtconfect@gmail.com>
2017-12-13 23:15:04 +0000

    Config changes.

--- conf/users.auth.php
+++ conf/users.auth.php
@@ -9,4 +9,6 @@
 # login:passwordhash:Real Name:email:groups,comma,seperated
 
 
-dtconfect:$1$ynzp3XYJ$JbIRoqYx8JYbeJhPzormJ1:Lark Irienys:dtconfect@gmail.com:admin,user
+#dtconfect:$1$ynzp3XYJ$JbIRoqYx8JYbeJhPzormJ1:Lark Irienys:dtconfect@gmail.com:admin,user
+admin:$1$5uI8Rjgj$2BlW8h1Zem47zBsAcZ3sO0:Admin:dtconfect@gmail.com:admin,user
+guest:$1$rGuNvX3f$bHhXEOgNeq5MnipPQH636.:Guest:dtconfect@gmail.com:user
